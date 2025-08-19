e4a9c42dca17247e31f7290486861364931e1afd
Valentina Velazquez <136092441+valuvel17@users.noreply.github.com>
2025-08-19 16:54:31 -0300

    changes on api

--- app/api/products/route.js
+++ app/api/products/route.js
@@ -1,8 +1,9 @@
 import Stripe from "stripe";
 import "../../../envConfig.js";
 
-const API_KEY = process.env.STRIPE_SECRET_KEY;
-const stripe = new Stripe(API_KEY);
+const API_KEY = process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY;
+const stripe = new Stripe(API_KEY, {
+  apiVersion: "2023-10-16"});
 
 export async function GET() {
   console.log("Clave de Stripe recibida:", process.env.STRIPE_SECRET_KEY);
